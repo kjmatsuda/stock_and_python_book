f0b921df6e3cca333ebd57f4ef220c0ae1eb22af
Koji Matsuda <kjmatsuda1985@gmail.com>
2021-06-13 13:20:13 +0900

    過去データの一括取得タイムアウト時のリトライ

--- chapter2/yahoo_csv_download.py
+++ chapter2/yahoo_csv_download.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
+MAX_RETRY_NUM = 10
+
 def download_stock_csv(code_range, save_dir):
 
     # CSVファイルを自動で save_dir に保存するための設定
@@ -20,10 +22,24 @@ def download_stock_csv(code_range, save_dir):
     # ここで手動でログインを行う。ログインしたら enter
     input('After login, press enter: ')
 
+    retry = 0
     for code in code_range:
         url = 'https://stocks.finance.yahoo.co.jp/stocks/history/?code={0}.T'.format(code)
-        driver.get(url)
 
+        while retry < MAX_RETRY_NUM:
+            try: 
+                driver.get(url)
+            except TimeoutException:
+                retry = retry + 1
+                print("Timeout: %(url)s" % {'url': url})
+                print("Retrying... (%(retry)s/%(max)s)" % {'retry': retry, 'max': MAX_RETRY_NUM})
+            else:
+                retry = 0
+                break
+
+        if retry >= MAX_RETRY_NUM:
+            raise TimeoutException()
+        
         try:
             # driver.find_element_by_css_selector('a.stocksCsvBtn').click()
             driver.find_element_by_css_selector('a.To_QbbNE').click()
@@ -33,5 +49,5 @@ def download_stock_csv(code_range, save_dir):
 if __name__ == '__main__':
     import os
     # download_stock_csv((7203, 9684), os.getcwd())
-g    download_stock_csv(range(7522,  9998), os.getcwd())
+    download_stock_csv(range(7696,  9998), os.getcwd())
     
