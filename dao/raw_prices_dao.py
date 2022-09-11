26ec2092ab9a8103875f76f8dae1089d59ec694d
Koji Matsuda <kjmatsuda1985@gmail.com>
2022-09-11 11:21:21 +0900

    pandas_datareader から取得したデータを raw_prices テーブルに登録できる ようにする

--- dao/raw_prices_dao.py
+++ dao/raw_prices_dao.py
@@ -16,3 +16,25 @@ class RawPricesDAO(object):
             if result != None:
                 latest_date = result[0]
             return latest_date
+
+    def insert(self, code, pdr_df):
+        price_generator = self.generate_price_from_pdr_df(code, pdr_df)
+
+        conn = sqlite3.connect(self.db_file_name)
+        with conn:
+            sql = """
+            INSERT INTO raw_prices(code,date,open,high,low,close,volume)
+            VALUES(?,?,?,?,?,?,?)
+            """
+            conn.executemany(sql, price_generator)
+
+    def generate_price_from_pdr_df(self, code, pdr_df):
+        for tuple in pdr_df.itertuples():
+            date = tuple[0].strftime('%Y-%m-%d')
+            open = round(float(tuple[3]), 2) # 初値
+            high = round(float(tuple[1]), 2) # 高値
+            low = round(float(tuple[2]), 2) # 安値
+            close = round(float(tuple[4]), 2) # 終値
+            volume = int(tuple[5]) # 出来高
+
+            yield code, date, open, high, low, close, volume
--- execute_insert_to_raw_prices.py
+++ execute_insert_to_raw_prices.py
@@ -1,5 +1,8 @@
 # pandas_datareader で株価データを取得し、raw_prices テーブルに登録する
 import os
+from datetime import datetime as dt, date, timedelta
+from pandas_datareader import data as pdr
+
 from dao.brands_dao import BrandsDAO
 from dao.raw_prices_dao import RawPricesDAO
 
@@ -11,5 +14,21 @@ raw_prices_dao = RawPricesDAO(db_file_name)
 code_list = brands_dao.get_brands_code_list()
 
 for code in code_list:
-    date = raw_prices_dao.get_latest_date_of_tick_code(code)
-    print('code:%s, 最新日時: %s' % (code, date))
+    if code == '7201':
+        latest_date_str = raw_prices_dao.get_latest_date_of_tick_code(code)
+        # print('code:%s, 最新日時: %s' % (code, latest_date_str))
+
+        # データ取得開始日
+        start='2000-01-01'
+
+        if latest_date_str != None:
+            latest_date = dt.strptime(latest_date_str, '%Y-%m-%d')
+            start = latest_date + timedelta(days = 1)
+
+        # データ取得終了日
+        end = '2021-06-15'
+
+        ticker_symbol = code + ".T"
+        pdr_df = pdr.get_data_yahoo(symbols=ticker_symbol, start=start, end=end, adjust_price=False)
+
+        raw_prices_dao.insert(code, pdr_df)
